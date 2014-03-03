d5fe8cb3a69e06d3435e0a94cf9176dc4d3ce408
Ryan <prk11@hotmail.com>
2014-03-03 01:09:17 +0000

    Change to an interface. Don't want it to be a class because you can't have just a DiagramElement.

--- src/ged/DiagramElement.java
+++ src/ged/DiagramElement.java
@@ -6,70 +6,24 @@
 
 package ged;
 
+import java.awt.Graphics;
+import java.awt.Point;
+
 /**
  *
  * @author Comp
  */
-public class DiagramElement
+public interface DiagramElement
 {
-  private Coordinate location;
   
-  public class Coordinate
-  {
-    private int x_coord, y_coord;
-    
-    public Coordinate(int X, int Y)
-    {
-      x_coord = X;
-      y_coord = Y;
-    }
-    
-    public int getX()
-    {
-      return x_coord;
-    }
-    
-    public int getY()
-    {
-      return y_coord;
-    }
-    
-    public void setX(int x)
-    {
-      x_coord = x;
-    }
-    
-    public void setY(int y)
-    {
-      y_coord = y;
-    }
-    
-    public void setCoordinates(Coordinate c)
-    {
-      x_coord = c.getX();
-      y_coord = c.getY();
-    }
-  }
+  public void draw(Graphics g);
   
-  public DiagramElement(int x, int y)
-  {
-    location = new Coordinate(x, y);
-  }
+  public Point getLocation();
   
-  public void draw()
-  {
-    
-  }
+  public void setLocation(Point loc);
   
-  public Coordinate getLocation()
-  {
-    return location;
-  }
-  
-  public void setLocation(Coordinate loc)
-  {
-    location = loc;
-  }
-          
+  public String getElementType();
+
+  public String getStringRepresentation();
   
 }
