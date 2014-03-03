006a860e2670e0f5c6f4f3c729737a194e2ed5db
Ryan <prk11@hotmail.com>
2014-03-03 01:14:02 +0000

    Added this in so everybody can access the getValueFromTag function that's needed by DiagramElements, Operations, Attributes, and Parameters. Little bit unclean to make it's own class for this, but seemed like the easiest way without using XML schema and complicating things.

---
+++ src/ged/Util.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ged;
+
+/**
+ *
+ * @author Comp
+ */
+public class Util
+{
+  public static String getValueFromTag(String str, String tag)
+  {
+    String val = "";
+    String startTag = "<" + tag + ">";
+    String endTag = "</" + tag + ">";
+    int startIndex = str.indexOf(startTag) + startTag.length();
+    int endIndex = str.indexOf(endTag);
+    if(endIndex > startIndex)
+      val = str.substring(startIndex, endIndex);
+    
+    return val;
+  }
+  
+}
